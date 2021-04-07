318063c4b29b39a0aeb5a4376b145b6882d1586a
Dragon-R <tlpeuu@gmail.com>
2021-04-06 20:24:34 -0400

    avance

--- index.php
+++ index.php
@@ -4,6 +4,14 @@
     <div class="container p-4">
         <div class="row">
             <div class="col-md-4">
+                <?php if (isset($_SESSION['message'])) { ?>
+                    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                        <?= $_SESSION['message'] ?>
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                <?php } ?>
                 <div class="card card-body">
                     <form action="save_task.php" method="POST">
                         <div class="form-group">
--- save_task.php
+++ save_task.php
@@ -1,5 +1,17 @@
 <?php
-if (isset($_POST['save_task'])) {
-    echo 'saving';
-}
+    include("db.php");
+    if (isset($_POST['save_task'])) {
+        $title = $_POST['title'];
+        $description = $_POST['description'];
+        
+        $query = "INSERT INTO task(title, description) VALUES ('$title', '$description')";
+        $result = mysqli_query($conn, $query);
+        if (!$result) {
+            die("Query Failed");
+        }
+        $_SESSION['message'] = 'Task Saved successfully';
+        $_SESSION['message_type'] = 'success';
+
+        header("Location: index.php");
+    }
 ?>
